945f28634b516c24bd1d8ee3d654b0246b50695e
wyhazq <443265447@qq.com>
2018-09-19 18:09:15 +0800

    update webVC

--- Kawagarbo/Core/ViewController/KGWebViewController.swift
+++ Kawagarbo/Core/ViewController/KGWebViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WebKit
 
 public class KGWebViewController: UIViewController {
     
@@ -24,6 +25,11 @@ public class KGWebViewController: UIViewController {
     public var callback: KGWebCallback?
     
     public var userInfo: [String: Any]?
+    
+    private lazy var jsBridge: KGWKJSBridge = {
+        let jsBridge = KGWKJSBridge(webView: self.webView!)
+        return jsBridge
+    }()
     
     public convenience init(urlString: String, parameters: [String: Any]? = nil, headers: [String: String]? = nil) {
         self.init()
@@ -53,5 +59,93 @@ public class KGWebViewController: UIViewController {
         super.viewDidDisappear(animated)
         
     }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
 
 }
+
+// MARK: - Setup
+extension KGWebViewController {
+    
+    func reloadWebView() {
+        
+    }
+    
+    func registNativeApi() {
+        
+    }
+    
+    func setupNavigatonController() {
+        
+    }
+    
+}
+
+
+// MARK: - KGWKWebViewDelegate
+extension KGWebViewController: KGWKWebViewDelegate {
+    
+    func webView(_ webView: KGWKWebView, shouldStartLoadWith request: URLRequest, navigationType: WKNavigationType) -> Bool {
+        
+        return true
+    }
+    
+    func webViewDidStartLoad(_ webView: KGWKWebView) {
+        
+    }
+    
+    func webViewDidFinishLoad(_ webView: KGWKWebView) {
+        
+    }
+    
+    func webView(_ webView: KGWKWebView, didFailLoadWithError error: Error) {
+        
+    }
+    
+    func webViewDidTerminate(_ webView: KGWKWebView) {
+        
+    }
+    
+}
+
+
+// MARK: - Action
+extension KGWebViewController {
+    
+    func showFailView(with error: Error) {
+        
+    }
+    
+    func updateWebRoute() {
+        
+    }
+    
+    func updateWebUI() {
+        
+    }
+    
+    func leftItemAction(_ sender: UIButton) {
+        
+    }
+    
+    func rightItemAction(_ sender: UIButton) {
+        
+    }
+    
+}
+
+extension KGWebViewController {
+    
+    func addNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(exitApp(_:)), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+    }
+    
+    @objc func exitApp(_ notification: Notification) {
+        
+    }
+    
+}
+
